ce2eef05044a50856e631cafc6994d3913c965cb
otlevire <eriveltoclenio@gmail.com>
2023-06-11 12:23:39 +0100

    Terceiro exercicio

---
+++ 1-Exercicios de Fundamentos/Exercicios2.3-Mostrar o algarismo das dezenas do numero.cpp
@@ -0,0 +1,25 @@
+/*
+** Enunciado: 2.3- Desenvolva um programa para ler um numero qualquer e 
+mostrar na tela o algarismo das dezenas.
+
+** Autor................: Erivelto Clenio da Costa e Silva.
+** Entrada..............: Um numero inteiro.
+** Saida................: A soma do triplo do sucessor pelo dobro do antecessor
+** Data de Implementacaoo: 11/06/2023.
+*/
+
+#include <stdio.h>
+
+int main()
+{
+	int numero, resultado;
+	
+	printf("Digite o numero:");
+	scanf("%d", &numero);
+	
+	resultado = numero%10;
+	
+	printf("O resultado eh: %d.\n", resultado);
+	getchar();
+	return 0;
+}
