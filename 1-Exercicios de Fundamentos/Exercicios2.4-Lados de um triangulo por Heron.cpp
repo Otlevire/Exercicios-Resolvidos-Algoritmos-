e9d02905a9a247104eef564dbc6b86b0f2b18351
otlevire <eriveltoclenio@gmail.com>
2023-06-11 12:26:23 +0100

    Exercicio 2.4

---
+++ 1-Exercicios de Fundamentos/Exercicios2.4-Lados de um triangulo por Heron.cpp
@@ -0,0 +1,36 @@
+/*
+** Enunciado: 2.4- Desenvolva um programa para ler os tres lados de um triangulo. 
+Calcular e mostrar na tela o valor da area. Para o efeito, utilize a formula 
+de Heron: Area = sqrt(s(s-a)(s-b)(s-c)), onde s = (a+b+c)/2;
+
+** Autor................: Erivelto Clenio da Costa e Silva.
+** Entrada..............: Os tres valores dos lados.
+** Saida................: O valor da area calculada.
+** Data de Implementa��o: 11/06/2023.
+*/
+
+#include <Math.h>
+#include <stdio.h>
+
+int main()
+{
+	float ladoA, ladoB, ladoC;
+	float area, s;
+	
+	printf("Digite o valor do lado A:");
+	scanf("%f", &ladoA);
+	
+	printf("Digite o valor do lado B:");
+	scanf("%f", &ladoB);
+	
+	printf("Digite o valor do lado C:");
+	scanf("%f", &ladoC);
+	
+	s = (ladoA + ladoB + ladoC)/2.0;
+	area = sqrt(s*(s-ladoA)*(s-ladoB)*(s-ladoC));
+	
+	printf("A area do triangulo e de: %.2f.\n", area);
+	
+	getchar();
+	return 0;
+}
